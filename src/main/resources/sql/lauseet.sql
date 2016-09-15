3c3ba7c06b816c965de0d5cb6c5bbfafbf7fb1ec
Jarno Luukkonen <luukkonen.jarno@gmail.com>
2016-09-15 18:35:30 +0300

    sql lataaminen tiedostosta

---
+++ src/main/resources/sql/lauseet.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Opiskelija (id integer PRIMARY KEY, nimi varchar(255));
+
+INSERT INTO Opiskelija (nimi) VALUES ('Platon');)
+INSERT INTO Opiskelija (nimi) VALUES ('Aristoteles');
+INSERT INTO Opiskelija (nimi) VALUES ('Homeros');
