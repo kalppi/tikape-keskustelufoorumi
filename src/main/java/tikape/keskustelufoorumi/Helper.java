c50b8c2704b464bb1c6dd2825e164882851d2b32
Jarno Luukkonen <luukkonen.jarno@gmail.com>
2016-10-10 20:38:50 +0300

    linkify

--- src/main/java/tikape/keskustelufoorumi/Helper.java
+++ src/main/java/tikape/keskustelufoorumi/Helper.java
@@ -4,6 +4,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Helper {
     public static String escapeString(String str) {
@@ -16,4 +21,34 @@ public class Helper {
             
         return date;
     }
+    
+    private static Pattern linkPattern = null;
+    
+    public static String linkify(String text) {
+        if(Helper.linkPattern == null) {
+            Helper.linkPattern = Pattern.compile("(?:(https?://)|(?:www\\.))[^\\s]+");
+        }
+        
+        Matcher m = Helper.linkPattern.matcher(text);
+        
+        StringBuffer result = new StringBuffer();
+
+        while(m.find()) {
+            MatchResult r = m.toMatchResult();
+            
+            String url = m.group(0);
+            if(m.group(1) == null) {
+                url = "http://" + url;
+            }
+            
+            url = "<a href=\"" + url + "\">" + m.group(0) + ">";
+            
+            m.appendReplacement(result, url);
+        }
+        
+        m.appendTail(result);
+        
+        return result.toString();
+    }
 }
+
